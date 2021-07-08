2ba673b4cf85e2c2e5a5937ffd6ec9d303b68547
xinnian.yang <xinnian.yang@seetatech.com>
2021-07-08 18:02:58 +0800

    '项目初始化'

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // 开发服务器配置
+  server: {
+    port: 11000,
+  }
+})
